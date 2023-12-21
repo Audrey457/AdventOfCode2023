17b00afad13f19d5433374c2f2a7e7c35aaf142e
Audrey457 <laudrey457@gmail.com>
2023-12-21 15:01:38 +0100

    modif day 1

---
+++ AdventOfCode2023/Day1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    public static class Day1
+    {
+        static Dictionary<string, char> dic = new Dictionary<string, char>
+            {
+                { "one", '1' },
+                { "two", '2' },
+                { "three", '3' },
+                { "four", '4' },
+                { "five", '5' },
+                { "six", '6' },
+                { "seven", '7' },
+                { "eight", '8' },
+                { "nine", '9' }
+            };
+        public static int GetCalibrationSum()
+        {
+            int total = 0;
+            var sw = new StreamWriter(@"C:\Users\310616706\source\repos\AdventOfCode2023\AdventOfCode2023\Output.txt");
+            using (var sr = new StreamReader(@"C:\Users\310616706\source\repos\AdventOfCode2023\AdventOfCode2023\Input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                    total += GetNumber(sr.ReadLine(), sw);
+                    sw.WriteLine(" | " + total);
+                }
+            }
+            sw.Dispose();
+            return total;
+        }
+
+        static char GetFirstFigure(string line)
+        {
+            int posFirst = line.Length;
+            char firstFigure = new char();
+            foreach (var item in dic.Keys)
+            {
+                if (line.Contains(item) && posFirst > line.IndexOf(item))
+                {
+                    firstFigure = dic.GetValueOrDefault(item);
+                    posFirst = line.IndexOf(item);
+                }
+            }
+            if(line.IndexOf(line.First(c => Char.IsNumber(c))) < posFirst)
+            {
+                firstFigure = line.First(c => Char.IsNumber(c));
+            }
+
+            return firstFigure;
+        }
+
+        static char GetLastFigure(string line)
+        {
+            int posLast = 0;
+            char lastFigure = new char();
+            foreach (var item in dic.Keys)
+            {
+                if (line.Contains(item) && posLast < line.IndexOf(item))
+                {
+                    lastFigure = dic.GetValueOrDefault(item);
+                    posLast = line.IndexOf(item);
+                }
+            }
+            if (line.IndexOf(line.First(c => Char.IsNumber(c))) > posLast)
+            {
+                lastFigure = line.First(c => Char.IsNumber(c));
+            }
+            return line.Last(c => Char.IsNumber(c));
+        }
+        static int GetNumber(string line, StreamWriter sw)
+        {
+            string newLine = GetNewLine(line);
+            int i = int.Parse(string.Concat(GetFirstFigure(line), GetLastFigure(line)));
+            sw.Write(newLine + " | " + i);
+            return i;
+        }
+
+        static string GetNewLine(string line)
+        {
+            string newLine = line;
+            int pos;
+            int posFirst = line.Length;
+            int posLast = 0;
+            string first = "";
+            string last = "";
+
+            Dictionary<string, int> dic = new Dictionary<string, int>
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 }
+            };
+            foreach (var ns in dic.Keys)
+            {
+                if (newLine.Contains(ns))
+                {
+                    pos = newLine.IndexOf(ns);
+                    if (pos < posFirst)
+                    {
+                        posFirst = newLine.IndexOf(ns);
+                        first = ns;
+                    }
+                    if (pos > posLast)
+                    {
+                        posLast = newLine.IndexOf(ns);
+                        last = ns;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(first))
+                newLine = newLine.Replace(first, dic.GetValueOrDefault(first).ToString());
+            if (!string.IsNullOrEmpty(last))
+                newLine = newLine.Replace(last, dic.GetValueOrDefault(last).ToString());
+            return newLine;
+        }
+    }
+}
